1d1b2f6689d607aa5851676cdd081b4d57509393
Mark Merritt <markmerritt804@gmail.com>
2019-04-12 18:43:03 -0400

    Add welcome controller

---
+++ spec/features/welcome_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+describe "navigation" do 
+  describe "index" do 
+    it "is reachable" do 
+      visit root_path
+      expect(page.status_code).to eq(200)
+    end
+  end
+end
