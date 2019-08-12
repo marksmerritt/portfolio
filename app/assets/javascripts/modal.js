5f8abef123d10aafb026da64611647589641028a
Mark Merritt <markmerritt804@gmail.com>
2019-08-12 19:51:53 -0400

    Add blank modals for all project features

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,19 +1,26 @@
 $(document).on("turbolinks:load", function(){
-  $("#contact-btn").click(openModal);
+  $(".modalable-link").each(function(i){
+    $(this).on("click", function(e){
+      if($(e.target).hasClass(`modalable-link--${i+1}`)){
+        openModal(i);        
+      }
+    });
+  });
 
   $(".modal__close-btn").click(closeModal);
 
-  $(document).click(function(e) {
+  $(document).click(function(e){
     if($(e.target).hasClass("modal")){
       closeModal();
     }
   });
 
-  function openModal(){
-    $("#contact-modal").css({"display": "flex", "visibility": "visible"});
+  function openModal(i){
+    console.log($(`#modal--${i+1}`));
+    $(`#modal--${i+1}`).css({"display": "flex", "visibility": "visible"});
   }
 
-  function closeModal(){
-    $("#contact-modal").css({"display": "none", "visibility": "hidden"});
+  function closeModal(i){
+    $(".modal").css({"display": "none", "visibility": "hidden"});
   }
 });
