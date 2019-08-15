adacc00b52d22453ef018e8393938af119bb6e75
Mark Merritt <markmerritt804@gmail.com>
2019-08-15 15:01:29 -0400

    Add modal content for UBG, PS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def display_icon(icon: svg_name, style: "", id: "")
+    path = asset_path('sprite.svg') 
+    icon = icon.prepend("#icon-")
+    
+    content_tag(:svg, class: style, id: id) do 
+      content_tag(:use, nil, 'xlink:href' => path + icon)
+    end
+  end
 end
