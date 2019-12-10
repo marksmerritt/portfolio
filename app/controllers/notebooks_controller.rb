e187a62eb8d8af998320de7d9c8dd22d24adc457
Mark Merritt <markmerritt804@gmail.com>
2019-12-10 13:11:14 -0500

    Add notebooks show action

--- app/controllers/notebooks_controller.rb
+++ app/controllers/notebooks_controller.rb
@@ -2,4 +2,8 @@ class NotebooksController < ApplicationController
   def index
     @notebooks = Notebook.all
   end
+
+  def show
+    @notebook = Notebook.find(params[:id])
+  end
 end
--- spec/features/notebooks_spec.rb
+++ spec/features/notebooks_spec.rb
@@ -1,15 +1,23 @@
 require "rails_helper"
 
 describe "navigation" do
-  describe "index" do 
-    before do 
-      @notebook1 = FactoryBot.create(:notebook)
-      @notebook2 = FactoryBot.create(:second_notebook)
-    end
+  before do 
+    @notebook1 = FactoryBot.create(:notebook)
+    @notebook2 = FactoryBot.create(:second_notebook)
+  end
 
+  describe "index" do 
     it "displays a list of notebooks" do 
       visit notebooks_path
       expect(page).to have_content(/#{@notebook1.name}|#{@notebook2.name}/)
     end
   end  
+
+  describe "show" do 
+    it "is reachable from index" do 
+      visit notebooks_path
+      click_link "notebook-show-#{@notebook1.id}"
+      expect(current_path).to eq(notebook_path(@notebook1))
+    end
+  end
 end
