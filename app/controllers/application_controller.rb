c14c9c239bf9ba51e4bde206ac54113890126ff6
Mark Merritt <markmerritt804@gmail.com>
2019-12-19 12:22:30 -0500

    Add seperate layout for notes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
+  layout :set_layout
+
+  protected 
+
+  def set_layout
+    controller_name == "welcome" ? "application" : "notes"
+  end
+
   def after_sign_in_path_for(resource)
     notebooks_path
   end
