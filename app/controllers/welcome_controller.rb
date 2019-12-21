e61b712fd01515ccf66f886a8f912f48db4684bf
Mark Merritt <markmerritt804@gmail.com>
2019-12-21 11:14:42 -0500

    Add redirect if user signed in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,13 @@
 class WelcomeController < ApplicationController
+  before_action :check_signed_in
+
   def index
     @message = Message.new
+  end
+
+  private
+
+  def check_signed_in
+    redirect_to notebooks_path if current_user.present?
   end
 end
