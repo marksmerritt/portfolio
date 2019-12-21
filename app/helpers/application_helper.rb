c0a49958e22be8c00b3758b483ef080a594b43d6
Mark Merritt <markmerritt804@gmail.com>
2019-12-21 15:43:59 -0500

    Add markdown/syntax highlight support for note body

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,36 @@ module ApplicationHelper
       content_tag(:use, nil, 'xlink:href' => path + icon)
     end
   end
+
+  #---------- Markdown/Syntax Highlighting ----------#
+
+  class CodeRayify < Redcarpet::Render::HTML
+    def block_code(code, language)
+      language ||= :ruby
+      CodeRay.scan(code, language).div
+    end
+  end
+
+  def markdown_to_html(text)
+    coderayified = CodeRayify.new(:filter_html => true, 
+                                  :hard_wrap => true)
+    options = md_options
+
+    markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+    markdown_to_html.render(text).html_safe
+  end
+
+
+  private
+
+  def md_options 
+    {
+      :fenced_code_blocks => true,
+      :no_intra_emphasis => true,
+      :autolink => true,
+      :strikethrough => true,
+      :lax_html_blocks => true,
+      :superscript => true
+    }
+  end
 end
